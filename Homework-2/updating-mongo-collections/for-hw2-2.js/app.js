8c4baa54846f209077a2606a1d72eb252e75297e
Kayode Alayode <ksamuel.chris@me.com>
2015-10-27 07:51:06 -0400

    added function to check if the array's State property is not empty and prints only the state and temp

--- Homework-2/updating-mongo-collections/for-hw2-2.js/app.js
+++ Homework-2/updating-mongo-collections/for-hw2-2.js/app.js
@@ -1,11 +1,28 @@
 // use weather;
 // db = weather;
 // db.data.find().sort({'State':1,'Temperature':-1}).limit(5).pretty();
+// db.data.count();
+
+
 
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     if(err) throw err;
-      console.log('connected to: weather');
-      db.close();
+    //  console.log('connected to: weather');
+
+    var grades = db.collection('data');
+
+    var state = '';
+    var cursor = grades.find({}).sort({'State':1,'Temperature':-1}).limit(10);
+
+
+    cursor.toArray(function(error,doc){
+      doc.forEach(function(element){
+         if(element["State"] !== state){
+        console.dir(element["State"]+': '+element["Temperature"]);
+        return db.close();
+      }
+    });
+  });
 });
