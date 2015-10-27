a90dacde871784215b4b885834c6bd564d72d27e
Kayode Alayode <ksamuel.chris@me.com>
2015-10-27 06:52:27 -0400

    writing the code to connect to weather

--- Homework-2/updating-mongo-collections/for-hw2-2.js/app.js
+++ Homework-2/updating-mongo-collections/for-hw2-2.js/app.js
@@ -1,32 +1,11 @@
-use weather;
-db = weather;
-db.data.find().sort({'State':1,'Temperature':-1}).limit(5).pretty();
+// use weather;
+// db = weather;
+// db.data.find().sort({'State':1,'Temperature':-1}).limit(5).pretty();
 
+var MongoClient = require('mongodb').MongoClient;
 
-// var MongoClient = require('mongodb').MongoClient;
-//
-// MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
-//     if(err) throw err;
-//
-//     var query ={};
-//
-//         db.collection('data').findOne(query, function(err, doc) {
-//         if(err) throw err;
-//         if(!doc) {
-//             console.log('No documents for assignment ' + query.assignment + ' found!');
-//             return db.close();
-//         }
-//         //
-//         // console.log(doc)
-//         // // query['_id'] = doc['_id'];
-//         // // doc['date_returned'] = new Date();
-//         //
-//         // db.collection('data').update(query, doc, function(err, updated) {
-//         //     if(err) throw err;
-//         //
-//         //     console.dir("Successfully updated " + updated + " document!");
-//         //
-//         //     return db.close();
-//         // });
-//     });
-// });
+MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
+    if(err) throw err;
+      console.log('connected to: weather');
+      db.close();
+});
