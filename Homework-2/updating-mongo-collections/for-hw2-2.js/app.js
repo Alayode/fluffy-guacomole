a24f95cbb521902e6bee9746079058babd858b88
Kayode Alayode <ksamuel.chris@me.com>
2015-10-27 06:37:49 -0400

    first commit

---
+++ Homework-2/updating-mongo-collections/for-hw2-2.js/app.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
+    if(err) throw err;
+
+    var query ={};
+
+        db.collection('data').findOne(query, function(err, doc) {
+        if(err) throw err;
+        if(!doc) {
+            console.log('No documents for assignment ' + query.assignment + ' found!');
+            return db.close();
+        }
+        //
+        // console.log(doc)
+        // // query['_id'] = doc['_id'];
+        // // doc['date_returned'] = new Date();
+        //
+        // db.collection('data').update(query, doc, function(err, updated) {
+        //     if(err) throw err;
+        //
+        //     console.dir("Successfully updated " + updated + " document!");
+        //
+        //     return db.close();
+        // });
+    });
+});
