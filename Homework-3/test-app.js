3f620ea1c85804ca1097f6778c578cc6a1e43c38
Kayode Alayode <ksamuel.chris@me.com>
2015-10-29 23:11:37 -0400

    fixed pages

--- Homework-3/test-app.js
+++ Homework-3/test-app.js
@@ -1,4 +1,12 @@
 use school;
 
-school = db;
-school.students.count()
+students = db.students;
+students.count();
+
+// what do we want?
+query = {_id : 137};
+
+//give me the query that is in the students collection in the school database oh and make it pretty!!!
+students.find(query).pretty();
+
+students.aggregate( { '$unwind' : '$scores' } , { '$group' : { '_id' : '$_id' , 'average' : { $avg : '$scores.score' } } } , { '$sort' : { 'average' : -1 } } , { '$limit' : 1 } )
