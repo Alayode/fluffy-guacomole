7fab6f237cd07b451972e559bd4c70c72a40ea93
Kayode Alayode <ksamuel.chris@me.com>
2015-11-04 16:02:46 -0500

    fixed script to use only WT db

--- Homework-4/bash.js
+++ Homework-4/bash.js
@@ -11,5 +11,6 @@ in the mongo shell using the bash to pipe the code into mongo
 killall mongod
 mkdir WiredTiger
 mongod -dbpath WT -storageEngine WiredTiger
-mongo // use WT
+mongo
+use WT
     db.foo.stats(); //info
